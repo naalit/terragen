905ebd7dc393256cff7f127e1b6f38b096b62e4b
Lorxu <sam@dirkback.com>
2020-04-03 13:41:44 -0500

    Molten stone is usable as fuel

--- src/main/scala/terragen/block/BlockRegistrizer.scala
+++ src/main/scala/terragen/block/BlockRegistrizer.scala
@@ -4,7 +4,10 @@ import net.minecraft.block._
 import net.minecraft.item._
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.furnace._
 import collection.mutable.ArrayStack
+import collection.mutable
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraft.world.biome.BiomeColors
@@ -14,14 +17,17 @@ import terragen._
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-
 object RegisterEverything {
   val LOGGER = LogManager.getLogger()
 
   var BLOCKS = ArrayStack[Block]()
   var FOLIAGE = ArrayStack[Block]()
   var ITEMS = ArrayStack[Item]()
+  var BURNS = mutable.HashMap[Item, Int]()
 
+  MinecraftForge.EVENT_BUS.addListener((evt: FurnaceFuelBurnTimeEvent) => {
+    BURNS.get(evt.getItemStack.getItem).map(evt.setBurnTime(_))
+  })
   FMLJavaModLoadingContext.get().getModEventBus().addListener((evt: RegistryEvent.Register[Block]) => {
     // This actually gets all register events
     // So we check for the name
@@ -68,15 +74,26 @@ trait BlockRegistrizer extends Block {
   var has_item = true
   var is_foliage = false
   var group = ItemGroup.BUILDING_BLOCKS
+  var burn_time = -1
 
   // You should call this statically or at least before registry events fire
   def register(name: String): BlockRegistrizer = {
+    val item = new BlockItem(this, new Item.Properties().group(group)).setRegistryName(name)
     this.setRegistryName(name)
+
     BLOCKS.push(this)
     if (has_item)
-      ITEMS.push(new BlockItem(this, new Item.Properties().group(group)).setRegistryName(name))
+      ITEMS.push(item)
     if (is_foliage)
       FOLIAGE.push(this)
+    if (burn_time >= 0)
+      BURNS += (item -> burn_time)
+
+    this
+  }
+
+  def fuel(ticks: Int): BlockRegistrizer = {
+    burn_time = ticks
     this
   }
 
--- src/main/scala/terragen/block/MoltenStone.scala
+++ src/main/scala/terragen/block/MoltenStone.scala
@@ -9,6 +9,11 @@ import net.minecraft.entity._
 import java.util.Random
 
 class MoltenStone extends Block(Block.Properties.create(Material.ROCK, MaterialColor.OBSIDIAN).lightValue(7).hardnessAndResistance(0.5f, 2.0f).tickRandomly()) with BlockRegistrizer {
+  // 40 items: 40% of a lava bucket, 2 dried kelp blocks, half a block of coal
+  // You could just place it, wait for it to melt, and put it in a bucket to more than double burn time
+  //   but that's a lot of work so it's fine
+  fuel(8000)
+
   override def ticksRandomly(state: BlockState) = true
   override def randomTick(state: BlockState, world: World, pos: BlockPos, rand: Random) = {
     world.setBlockState(pos, Blocks.LAVA.getDefaultState, 3)
